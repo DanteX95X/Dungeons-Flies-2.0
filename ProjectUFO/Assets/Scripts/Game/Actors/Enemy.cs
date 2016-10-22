97fd005551050871f04289d0fff085ee223f73e6
dante <daniellewinski95@gmail.com>
2016-10-22 15:21:50 +0200

    Enemy stub.

---
+++ ProjectUFO/Assets/Scripts/Game/Actors/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Assets.Scripts.Game.Actors
+{
+	public class Enemy : Actor
+	{
+		public override void ExecuteNextMove()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void RewindTime()
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+}
+
