3a0d783370d0f7629c3c7ea3baf92a87f80b4618
mikolajqc <mikolajqc@gmail.com>
2016-10-22 19:55:37 +0200

    Separated class from one file.
    New files:
    PriorityQueue.cs

--- ProjectUFO/Assets/Scripts/PathFinding.cs
+++ ProjectUFO/Assets/Scripts/PathFinding.cs
@@ -3,6 +3,7 @@ using System;
 using UnityEngine;
 using Assets.Scripts.Game.Map;
 using Assets.Scripts.Game;
+using Assets.Scripts.Utilities;
 
 namespace Assets.Scripts
 {
@@ -16,10 +17,9 @@ namespace Assets.Scripts
 				return new List<Field> ();
 			}
 
-			//#region variables
+			#region variables
 
 			Field currentField = startField;
-		//	Field goalField = Level.Grid [goalPosition];
 
 			HashSet<Field> visited = new HashSet<Field> ();
 			HashSet<Field> notVisited = new HashSet<Field> ();
@@ -29,7 +29,7 @@ namespace Assets.Scripts
 
 			PriorityQueue<Field> frontier = new PriorityQueue<Field> ();
 
-			//#endregion
+			#endregion
 
 			frontier.Push (currentField, 0);
 			costs [currentField] = 0;
@@ -42,9 +42,7 @@ namespace Assets.Scripts
 				if (currentField == goalField)
 				{
 					Debug.Log ("goalField reached!");
-					Debug.Log ("Position: " + currentField.transform.position.x + " " + currentField.transform.position.y);
 					return GetPath(cameFrom, goalField, startField);
-
 				}
 
 				foreach (Field neighbour in currentField.Neighbours.Values)
@@ -71,7 +69,6 @@ namespace Assets.Scripts
 							frontier.Push (neighbour, costs[neighbour]);
 						}
 
-						//Debug.Log ("Sasiad: " + neighbour.transform.position.x + " " + neighbour.transform.position.y);
 					}
 				}
 
@@ -111,123 +108,6 @@ namespace Assets.Scripts
 			return heuristic;
 		}
 
-	}
-
-	public class Pair<T1, T2>
-	{
-		public T1 first { get; set; }
-		public T2 second { get; set; }
-
-
-		public Pair()
-		{
-
-		}
-
-		public Pair(T1 first, T2 second)
-		{
-			this.first = first;
-			this.second = second;
-		}
-
-	}
-
-	public class PriorityQueue<T>
-	{
-		List<Pair<T, double>> nodes { get; set; }
-
-		public int Count { get { return nodes.Count; } }
-
-		public Pair<T, double> Top
-		{
-			get
-			{
-				if (nodes.Count < 2)
-					throw new IndexOutOfRangeException();
-				return nodes[1];
-			}
-		}
-
-		public PriorityQueue()
-		{
-			nodes = new List<Pair<T, double>>();
-			nodes.Add(null); //simplifies parent-child relations
-		}
-
-		void Swap(int first, int second)
-		{
-			Pair<T, double> temp = nodes[first];
-			nodes[first] = nodes[second];
-			nodes[second] = temp;
-		}
-
-		public void Push(T value, double priority)
-		{
-			int currentIndex = nodes.Count;
-			int parentIndex = currentIndex / 2;
-			nodes.Add(new Pair<T, double>(value, priority));
-
-			while (nodes[parentIndex] != null)
-			{
-				if (nodes[currentIndex].second > nodes[parentIndex].second)//if current has lower priority (higher weight in our case) than parent
-					break;
-
-				Swap(currentIndex, parentIndex);
-				currentIndex = parentIndex;
-				parentIndex = currentIndex / 2;
-			}
-		}
-
-		public T Pop()
-		{
-			if (nodes.Count < 2)
-				throw new IndexOutOfRangeException();
-
-			T result = nodes[1].first;
-			nodes.RemoveAt(1); //remove root
-
-			Rebuild();
-
-			return result;
-		}
-
-		void Rebuild()
-		{
-			nodes.Insert(1, nodes[nodes.Count - 1]); //insert last leaf in place of root
-			nodes.RemoveAt(nodes.Count - 1);
-
-			int currentIndex = 1;
-			int leftIndex = 2 * currentIndex;
-			int rightIndex = 2 * currentIndex + 1;
-
-			while (leftIndex < nodes.Count)
-			{
-
-
-				if (nodes[currentIndex].second > nodes[leftIndex].second && rightIndex >= nodes.Count)
-				{
-					Swap(currentIndex, leftIndex);
-					currentIndex = leftIndex;
-					leftIndex = 2 * currentIndex;
-					rightIndex = 2 * currentIndex + 1;
-				}
-				else if (rightIndex < nodes.Count)
-				{
-					int childIndex = nodes[leftIndex].second <= nodes[rightIndex].second ? leftIndex : rightIndex;
-					if (nodes[currentIndex].second > nodes[childIndex].second)
-					{
-						Swap(currentIndex, childIndex);
-						currentIndex = childIndex;
-						leftIndex = 2 * currentIndex;
-						rightIndex = 2 * currentIndex + 1;
-					}
-					else
-						break;
-				}
-				else
-					break;
-			}
-		}
 	}
 
 }
