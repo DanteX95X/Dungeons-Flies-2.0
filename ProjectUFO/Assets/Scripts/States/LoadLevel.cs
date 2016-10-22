f3a37df4e678b8a59220076a01a6b6890a16e717
dante <daniellewinski95@gmail.com>
2016-10-22 21:07:14 +0200

    Setting camera so it will see entire level.

--- ProjectUFO/Assets/Scripts/States/LoadLevel.cs
+++ ProjectUFO/Assets/Scripts/States/LoadLevel.cs
@@ -38,12 +38,26 @@ namespace Assets.Scripts.States
 			GameObject grid = new GameObject();
 			grid.name = "grid";
 
+			Vector2 maxCoordinates = new Vector2(float.MinValue, float.MinValue);
+			Vector2 minCoordinates = new Vector2(float.MaxValue, float.MaxValue);
+
 			foreach (var fieldInfo in level.Grid)
 			{
 				GameObject tile = Instantiate(field, fieldInfo.first, Quaternion.identity) as GameObject;
 				tile.transform.parent = grid.transform;
 				Game.Game.Instance.CurrentLevel.Grid[tile.transform.position] = tile.GetComponent<Field>();
+
+				if (tile.transform.position.x < minCoordinates.x)
+					minCoordinates.x = tile.transform.position.x;
+				if (tile.transform.position.x > maxCoordinates.x)
+					maxCoordinates.x = tile.transform.position.x;
+				if (tile.transform.position.y < minCoordinates.y)
+					minCoordinates.y = tile.transform.position.y;
+				if (tile.transform.position.y > maxCoordinates.y)
+					maxCoordinates.y = tile.transform.position.y;
 			}
+
+			Debug.Log(maxCoordinates + " " + minCoordinates);
 
 			foreach (Field field in Game.Game.Instance.CurrentLevel.Grid.Values)
 			{
@@ -58,12 +72,35 @@ namespace Assets.Scripts.States
 				}
 
 			}
-
+				
 			GameObject playersParent = new GameObject();
 			playersParent.name = "players";
 			GameObject newPlayer = Instantiate(player, level.PlayersPosition, Quaternion.identity) as GameObject;
 			Game.Game.Instance.CurrentLevel.ActivePlayer = newPlayer.GetComponent<Player>();
 			newPlayer.transform.parent = playersParent.transform;
+
+			SetCamera(new Vector2[] { maxCoordinates, minCoordinates});
+		}
+
+		void SetCamera(Vector2[] positions)
+		{
+			Camera mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>(); 
+			Vector3 desiredPosition = new Vector3((positions[0].x + positions[1].x)/2, (positions[0].y + positions[1].y)/2, -10);
+			mainCamera.transform.position = desiredPosition;
+
+
+			Vector3 desiredLocalPosition = mainCamera.transform.InverseTransformPoint(desiredPosition);
+			float size = 0;
+			foreach (Vector3 position in positions)
+			{
+				Vector3 targetLocalPosition = mainCamera.transform.InverseTransformPoint(position);
+				Vector3 desiredPositionToTarget = targetLocalPosition - desiredLocalPosition;
+
+				size = Mathf.Max(size, Mathf.Abs(desiredPositionToTarget.y));
+				size = Mathf.Max(size, Mathf.Abs(desiredPositionToTarget.x) / mainCamera.aspect);
+			}
+			size += 1;
+			mainCamera.orthographicSize = size;
 		}
 	}
 }
